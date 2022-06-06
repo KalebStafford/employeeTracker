e205de0aa2914cb1097387b1048998dfa1c9a03b
Kaleb Stafford <kalebstafford0@gmail.com>
2022-06-06 00:06:09 -0500

    finish seed

--- sql/seed.sql
+++ sql/seed.sql
@@ -17,3 +17,19 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Software Engineer", 100000, 4);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Legal Team Lead", 240000, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Josue", "Paniagua", 4, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Adrian", "Castillo", 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Hailee", "Judson", 2, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Matt", "Reece", 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Isabell", "Chartier", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sydney", "Rosenkrantz", 3, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Caleb", "Bentley", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Aimee", "Baldwin", 3, 4);
