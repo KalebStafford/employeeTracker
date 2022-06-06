61073ebbda5ae5fc2e89f9685d1f26ab8afe3c57
Kaleb Stafford <kalebstafford0@gmail.com>
2022-06-06 07:32:40 -0500

    dataPrompt

--- index.js
+++ index.js
@@ -11,3 +11,45 @@ connection.connect((err) => {
   if (err) throw err;
   dataPrompt();
 });
+function dataPrompt() {
+  inquirer
+    .prompt({
+      message: "What do you plan to do?",
+      choices: [
+        "View Employees",
+        "View Employees (by Department)",
+        "Add Employees",
+        "Remove Employees",
+        "Update Employees Role",
+        "Add Roles",
+        "Exit",
+      ],
+      name: "task",
+      type: "list",
+    })
+    .then(({ task }) => {
+      switch (task) {
+        case "View Employees":
+          viewEmployeesTask();
+          break;
+        case "View Employees (by Department)":
+          viewEmpByDepoTask();
+          break;
+        case "Add Employees":
+          addEmployees();
+          break;
+        case "Remove Employees":
+          removeEmployees();
+          break;
+        case "Update Employees Role":
+          updateEmpRole();
+          break;
+        case "Add Roles":
+          addRoles();
+          break;
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+}
