5e02eb10613d491c28999d35786c5872ebd5ed87
Kaleb Stafford <kalebstafford0@gmail.com>
2022-06-05 21:10:21 -0500

    employeedb start + department table

---
+++ sql/schema.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS employeesDB;
+CREATE DATABASE employeesDB;
+USE employeesDB;
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
